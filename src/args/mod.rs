ab582e28df9fa615d0970bfb104a96f96eadf17c
Ev357 <evest.dev@gmail.com>
2025-11-15 17:28:53 +0100

    feat: added helps

--- src/args/mod.rs
+++ src/args/mod.rs
@@ -13,15 +13,27 @@ pub struct Args {
     #[command(flatten)]
     pub template: Option<TemplateArgs>,
 
-    #[arg(short, long, conflicts_with = "template")]
+    #[arg(
+        short,
+        long,
+        conflicts_with = "template",
+        help = "Generates shell completions"
+    )]
     pub generate: bool,
 }
 
 #[derive(clap::Args, Debug)]
 #[group(id = "template")]
 pub struct TemplateArgs {
+    #[arg(help = "The template to use")]
     pub template_name: Template,
 
-    #[arg(default_value = ".", value_parser = path_parser, value_hint = ValueHint::DirPath, requires = "template_name")]
+    #[arg(
+        default_value = ".",
+        value_parser = path_parser,
+        value_hint = ValueHint::DirPath,
+        requires = "template_name",
+        help = "The path where to store the template"
+    )]
     pub path: PathBuf,
 }
