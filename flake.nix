248a4e1be01bdba8c1739525ebf516d2ec7ac373
Ev357 <evest.dev@gmail.com>
2025-11-16 20:33:14 +0100

    build: cachix cache

--- flake.nix
+++ flake.nix
@@ -3,9 +3,11 @@
 
   nixConfig = {
     extra-substituters = [
+      "https://ev357.cachix.org"
       "https://nix-community.cachix.org"
     ];
     extra-trusted-public-keys = [
+      "ev357.cachix.org-1:bI65rULXWJ8IMM+tosc/Z+9W53nL6uj4+5FLXX6BN3Q="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
