fb4f6ede749a64729c0bb52448f03a4fd3d9cac7
Ev357 <evest.dev@gmail.com>
2025-11-16 19:50:43 +0100

    feat: added simple-haskell template

--- src/template.rs
+++ src/template.rs
@@ -5,12 +5,14 @@ use clap::ValueEnum;
 #[derive(ValueEnum, Clone, Debug)]
 pub enum Template {
     Rust,
+    SimpleHaskell,
 }
 
 impl Display for Template {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Template::Rust => write!(f, "rust"),
+            Template::SimpleHaskell => write!(f, "simple-haskell"),
         }
     }
 }
---
+++ templates/simple-haskell/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "simple-haskell-template";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
+  in {
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          ghc
+        ];
+      };
+    });
+  };
+}
